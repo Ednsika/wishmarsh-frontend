5210202d69c146f8a4e94e4e00ab99e3ab218a70
Ednsika <105194138+Ednsika@users.noreply.github.com>
2025-07-25 15:39:48 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-pink-500 to-black text-white p-6">
+      <h1 className="text-4xl font-bold mb-4 text-center">Let’s make your day a special one</h1>
+      <button className="mt-4 px-6 py-3 bg-white text-black font-semibold rounded-xl shadow hover:bg-gray-200 transition">Create an Event</button>
+    </main>
+  );
+}
